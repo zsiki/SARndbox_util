4abc13994de98e5e9ed1689e372e21dcc2e8a154
zsiki <siki1958@gmail.com>
2017-01-12 01:29:09 +0100

    Multiple imput file format support added

--- dem2grid.py
+++ dem2grid.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 """
-    Convert USGS ASCII DEM file to SARnbox grid format
+    Convert GDAL supported single band DEM file to SARnbox grid format
     (c) Zoltan Siki siki.zoltan@epito.bme.hu
     GPL 2
 
@@ -9,31 +9,48 @@
         or
         dem2grid.py input.dem [output.grid]
 
-    Input file is a USGS ASCII DEM file, you can create it from SARndbox
+    Input file is a GDAL suported DEM file, you can create it from SARndbox
     (Save Bathimetry) or several open source software can be used e.g. QGIS.
     Output file name is optional, default is the name of the input file.
+    Some supported input data formats:
+        Name                              Extension
+        GTiff (GeoTiff single band)       tif
+        USGSDEM (USGS DEM)                dem
+        GRASSASCIIGrid (GRASS ASCII GRID) arx
+        AAIGrid (ESRI ASCII GRID)         asc
+        ...
 """
-
 import sys
 import os
-import osgeo.gdal
-from gdalconst import *
+import osgeo.gdal as gd
+from gdalconst import GA_ReadOnly, GDT_Byte, GDT_UInt16, GDT_Int16, \
+     GDT_UInt32, GDT_Int32, GDT_Float32, GDT_Float64
 import struct
 
+# GDAL data types to packt data_types
+gd_type = {GDT_Byte:    "b",
+           GDT_UInt16:  "H",
+           GDT_Int16:   "h",
+           GDT_UInt32:  "I",
+           GDT_Int32:   "i",
+           GDT_Float32: "f",
+           GDT_Float64: "d"}
 # check command line parameters
 if len(sys.argv) <= 1:
     print("Usage: dem2grid.py input.dem [output.grid]")
-    sys.exit(1)
-ifilename = sys.argv[1]
+    #sys.exit(1)
+    ifilename = "../SARndbox-2.3/bin/test.asc"
+else:
+    ifilename = sys.argv[1]
 # generate output file name if not given on the command line
 if len(sys.argv) > 2:
     ofilename = sys.argv[2]
 else:
     ofilename = os.path.splitext(ifilename)[0] + ".grid"
-# use gdal to read USGS DEM file
-idataset = osgeo.gdal.Open(ifilename, GA_ReadOnly)
+# use gdal to read DEM file
+idataset = gd.Open(ifilename, GA_ReadOnly)
 if idataset is None:
-    print("Cant read input file");
+    print("Cannot read input file {}".format(ifilename));
     sys.exit(2)
 # get size of dem
 cols = idataset.RasterXSize
@@ -49,5 +66,11 @@ of = open(ofilename, "wb")
 of.write(struct.pack("2i", cols, rows))
 of.write(struct.pack("4f", xul, ylr, xlr, yul))
 band = idataset.GetRasterBand(1)
-of.write(band.ReadRaster(0, 0, cols, rows, cols, rows, band.DataType))
+d = band.ReadRaster(0, 0, cols, rows, cols, rows, band.DataType)
+data = struct.unpack(gd_type[band.DataType] * (rows * cols), d)
+print(band.DataType)
+print(len(data))
+print(type(data))
+print(data)
+of.write(struct.pack("f" * (cols * rows), *data))
 of.close()
